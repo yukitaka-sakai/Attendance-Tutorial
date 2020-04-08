87607acd8b6686849bd4c66a0c0ebca2e4f9af45
設定したいユーザー名をここに入力してください <yasm1112@yahoo.co.jp>
2020-04-08 22:11:51 +0000

    4章5.1終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX }, 
                     uniqueness: true 
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
