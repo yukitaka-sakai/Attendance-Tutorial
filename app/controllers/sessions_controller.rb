0de66d1365b4952bd56157ca457796c745ad6fbb
設定したいユーザー名をここに入力してください <yasm1112@yahoo.co.jp>
2020-04-13 12:54:11 +0000

    6章3.2終了

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
-  
+
   def new
   end
-  
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
