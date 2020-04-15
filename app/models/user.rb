22ca96963546f29d793e7fa83679fdb97364c865
設定したいユーザー名をここに入力してください <yasm1112@yahoo.co.jp>
2020-04-15 21:11:37 +0000

    Finish full-fledged-login

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,6 +36,8 @@ class User < ApplicationRecord
 
   # トークンがダイジェストと一致すればtrueを返します。
   def authenticated?(remember_token)
+    # ダイジェストが存在しない場合はfalseを返して終了します。
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
   
