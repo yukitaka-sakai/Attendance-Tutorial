6fe5c7826358faf36992f2fdd520f817358bc5dd
設定したいユーザー名をここに入力してください <yasm1112@yahoo.co.jp>
2020-05-02 15:30:29 +0000

    10章8終了

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -9,4 +9,9 @@ module AttendancesHelper
     # どれにも当てはまらなかった場合はfalseを返します。
     false
   end
+  
+  # 出勤時間と退勤時間を受け取り、在社時間を計算して返します。
+  def working_time(start, finish)
+    format("%.2f", (((finish - start) / 60)  / 60.0))
+  end
 end
